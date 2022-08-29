bdfd7e7eb6afb8f011cf9056835c644c2f6ca3f9
osvaldo <iosvaldo.app@gmail.com>
2022-08-28 21:10:03 -0400

    commit tree 146cfb8c281fd44dab99a0e4d9b1850041691c43

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // code your solution here
-function saturdayFun(activity = "roller-skate"){
-    return (`This Saturday, I want to ${activity}!`)
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  
 }
-function mondayWork(activity = "go to the office"){
-    return(`This Monday, I will ${activity}.`)
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
 }
 
-function wrapAdjective(name= "*") {
-   const first = "You are"
-    return function innerFunction(name2 = "special"){
-return `${first} ${name}${name2}${name}!`
-    };
+function wrapAdjective(greet = "*") {
+    const first = "You are"
+    return function(param ="special") {
+        return `${first} ${greet}${param}${greet}!`;
+    }
 }
+wrapAdjective("*")("programmer");
